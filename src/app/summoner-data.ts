8dc1f2820c4cd82b96bbeb890e3ce107d8b9b84b
JamesVal <james.jo.val@gmail.com>
2018-01-09 09:20:27 -0800

    Fixed testObj

--- src/app/summoner-data.ts
+++ src/app/summoner-data.ts
@@ -9,4 +9,4 @@ export interface SummonerDTO {
   accountId: number;
 }
 
-export const testObj = {"id":67972542,"accountId":228054520,"name":"ShuntStick","profileIconId":982,"revisionDate":1515464902000,"summonerLevel":45};
+export const testObj = {"id":67972542,"accountId":228054520,"name":"ShuntStick","profileIconId":982,"revisionDate":1515464902000,"summonerLevel":46};
