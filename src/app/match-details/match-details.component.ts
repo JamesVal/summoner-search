645da87cd629faa319bb25e4b05e88edf902e205
JamesVal <james.jo.val@gmail.com>
2018-01-14 21:08:48 -0800

    Continued match data support

--- src/app/match-details/match-details.component.ts
+++ src/app/match-details/match-details.component.ts
@@ -14,7 +14,21 @@ export class MatchDetailsComponent implements OnInit {
 
   // Decided not to define the API response since the order it actually responds in doesn't match the order specified in their documentation - so now I just grab whatever object that is returned
   recentMatchData: any;
-   
+  matchDataList: any[];  
+  currentMatchIdx: number;
+  
+  getMatchData(matchId: number): void {
+    console.log('get match data: ' + matchId);
+	this.summonerService.getMatchDetails(matchId).subscribe(matchData => {
+	  console.log(matchData);
+	});
+  }	  
+  
+  getAllMatchData(): void {
+    this.currentMatchIdx = 0;
+	this.getMatchData(this.recentMatchData.matches[this.currentMatchIdx].gameId);
+  }
+  
   updateMatchData(): void {
     var summonerName = this.route.parent.snapshot.paramMap.get('name');
 	console.log('summoner_name:' + summonerName);
@@ -24,14 +38,16 @@ export class MatchDetailsComponent implements OnInit {
         this.recentMatchData = recentMatchData;
 		
 		// JJV DEBUG
-		console.log('got match data');
+		console.log('get recent match list');
 		
-
-        // JJV DEBUG - may need to do a recursive subscribe of sort right here		
+        // JJV DEBUG - may need to do a recursive subscribe of sort right here
+		this.getAllMatchData();
+/*
 		for (var eachMatchIdx = 0; eachMatchIdx < this.recentMatchData.matches.length; eachMatchIdx++) {
           //this.summonerService.getMatchDetails
 		  console.log(this.recentMatchData.matches[eachMatchIdx].gameId);
 		}
+*/
 	  })
 	});
 
